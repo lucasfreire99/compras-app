f032e3da7f23b1f70880e9512f6da8443c166a01
lucasfreire99 <fran.freire043@gmail.com>
2026-03-02 13:43:22 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,18 @@
-const CACHE_NAME = "lista-sistema-v1";
+const CACHE_NAME = "compras-app-v1";
 
-const urlsToCache = [
-  "/",
-  "index.html",
-  "style.css",
-  "script.js",
-  "manifest.json"
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
@@ -27,11 +20,16 @@ self.addEventListener("activate", event => {
     caches.keys().then(keys =>
       Promise.all(
         keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
+          if (key !== CACHE_NAME) return caches.delete(key);
         })
       )
     )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
